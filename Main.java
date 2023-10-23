54d4bb48ef7bdf9f996376a0749ccbcd5f981c1c
diegocortes4 <diegocortes4@gmail.com>
2023-10-23 20:27:12 +0000

    MaximumDecline

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
-
 public class Main {
 
   public static void main(String[] args) throws IOException {
